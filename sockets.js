3f84c1e61867c9f466e3b97761a4d5feea9c9b3b
fahimtakin <fahim.tanzil@hotmail.com>
2023-07-10 01:37:44 +0600

    add rooms

--- sockets.js
+++ sockets.js
@@ -2,27 +2,32 @@ let readyPlayerCount = 0;
 
 function listen(io) {
   io.on("connection", (socket) => {
+    let room;
     console.log("User conncected, ID:", socket.id);
 
     socket.on("ready", () => {
-      console.log("Player ready, ID:", socket.id);
+      room = "room" + Math.floor(readyPlayerCount / 2);
+      socket.join(room);
+
+      console.log("Player ready, ID:", socket.id, ", Room:", room);
       readyPlayerCount++;
 
       if (readyPlayerCount % 2 === 0) {
-        io.emit("startGame", socket.id);
+        io.in(room).emit("startGame", socket.id);
       }
     });
 
     socket.on("paddleMove", (paddleData) => {
-      socket.broadcast.emit("paddleMove", paddleData);
+      socket.to(room).emit("paddleMove", paddleData);
     });
 
     socket.on("ballMove", (ballData) => {
-      socket.broadcast.emit("ballMove", ballData);
+      socket.to(room).emit("ballMove", ballData);
     });
 
     socket.on("disconnect", (reason) => {
       console.log(`Client ${socket.id} disconnected: ${reason}`);
+      socket.leave(room);
     });
   });
 }
